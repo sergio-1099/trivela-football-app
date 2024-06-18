4f3266f55f4caf82d4dcb9fd695e7d8d106edbaf
sergio-1099 <sergioguerra6188@gmail.com>
2024-06-18 13:28:00 -0400

    Create Nav bar that updates state

--- trivela-football-react/src/App.jsx
+++ trivela-football-react/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [leagueSelection, setLeagueSelection] = React.useState([
+    { league: 'Premier League', selected: true },
+    { league: 'La Liga', selected: false },
+    { league: 'Bundesliga', selected: false },
+    { league: 'Serie A', selected: false },
+    { league: 'Ligue 1', selected: false }, 
+    { league: 'Champions League', selected: false },
+    { league: 'EUROs', selected: false },
+    { league: 'World Cup', selected: false }
+  ])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <Nav leagueSelection={leagueSelection} setLeagueSelection={setLeagueSelection} />
 }
 
 export default App
---
+++ trivela-football-react/src/components/Nav.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+function Nav(props) {
+  const leagueElements = props.leagueSelection.map(league => {
+    const leagueClass = league.selected ? "nav-button selected" : "nav-button"
+    return (
+      <li className="nav-list-element" key={league.league}>
+        <button className={leagueClass} onClick={() => handleClick(league.league)}>
+          {league.league}
+        </button>
+      </li>
+    )
+  })
+
+  function handleClick(leagueName) {
+    const oldSelect = props.leagueSelection.findIndex(obj => obj.selected)
+    const selectIdx = props.leagueSelection.findIndex(obj => obj.league === leagueName)
+
+    props.setLeagueSelection(prevSelection => {
+      const array = [...prevSelection]
+      array[oldSelect].selected = false
+      array[selectIdx].selected = true;
+      return array;
+    })
+  }
+
+  return (
+    <nav>
+      <h1>Trivela</h1>
+
+      <h2 className="competition-cat">Domestic Leagues</h2>
+      <ul className="nav-list">
+        {leagueElements[0]}
+        {leagueElements[1]}
+        {leagueElements[2]}
+        {leagueElements[3]}
+        {leagueElements[4]}
+
+      </ul>
+
+      <h2 className="competition-cat">European Leagues</h2>
+      <ul className="nav-list">
+        {leagueElements[5]}
+      </ul>
+
+      <h2 className="competition-cat">International Leagues</h2>
+      <ul className="nav-list">
+        {leagueElements[6]}
+        {leagueElements[7]}
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
