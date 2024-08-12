a3b9533e42968dd880cfa012a1149044c48fcc6c
sergio-1099 <sergioguerra6188@gmail.com>
2024-08-12 15:34:56 -0400

    Add vite config file

--- trivela-football-react/vite.config.js
+++ trivela-football-react/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.football-data.org', // The base URL of the API you are requesting from
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, ''), // Rewrites the URL path, removing '/api'
+      },
+    },
+  },
 })
